c90a5f7fb6c98a8729c744ad0491a237e9aa4918
MMMJENHI <155253082+MMMJENHI@users.noreply.github.com>
2026-04-15 18:22:03 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// [https://vite.dev/config/](https://vite.dev/config/)
 export default defineConfig({
   plugins: [react()],
   // 🚀 CETTE LIGNE EST LE "GPS" : Elle dit à GitHub de chercher les fichiers
