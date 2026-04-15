d08852e6239e570abb9f4ecc56e4b0f7a135efc7
MMMJENHI <155253082+MMMJENHI@users.noreply.github.com>
2026-04-15 18:15:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🚀 CETTE LIGNE EST LE "GPS" : Elle dit à GitHub de chercher les fichiers
+  // dans le dossier /mon-labo-rlc/ et non à la racine.
+  base: '/mon-labo-rlc/', 
 })
